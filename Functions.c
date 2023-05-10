7b3a6a71422c488b7f8244cfa0286c3803dbb96c
Zzlatoslava <zlatoslava0215@gmail.com>
2023-05-10 23:02:47 +0300

    ...

--- Functions.c
+++ Functions.c
@@ -4,25 +4,28 @@
 
 #include "Functions.h"
 
-
-
 void save_img (const char* path, BMP img ){
-    FILE *f = fopen(path , "wb");
+    FILE *f = fopen(path, "wb");
     if (f == NULL) {
-        printf("Unable to open image\n");
-
+        printf("Invalid file path\n");
     }
-    fwrite(&img.file_header, 1, sizeof(img.file_header), f);
-    fwrite(&img.info, 1, sizeof(img.info), f);
-    for (int i = 0; i < sizeof(img.info.height);  i++){
-        fwrite(&img.data[i], 1, sizeof(img.data)*((img.info.height*3-3)&(-4)), f);
+    else{
+        fwrite(&img.file_header, 1, sizeof(img.file_header), f);
+        fwrite(&img.info, 1, sizeof(img.info), f);
+
+        for (int i = 0; i < sizeof(img.info.height);  i++){
+            fwrite(&img.data[i], 1, sizeof(img.data)*((img.info.height*3-3)&(-4)), f);
+        }
+        printf("Работает!\n");
+
+
     }
     fclose(f);
-    printf("Работает!\n");
+
 }
 
-BMP open_img (const char* path){
-    BMP img;
+
+BMP open_img(const char* path){
     FILE *f = fopen(path, "rb");
     if (f == NULL) {
         printf("Unable to open image\n");
@@ -31,26 +34,27 @@ BMP open_img (const char* path){
     else{
         printf("Image is open\n");
     }
+
+    BMP img;
     fread(&img.file_header, 1, sizeof(img.file_header), f);
-    if (img.file_header.type[0] != 19778 && img.file_header.type[0] != 9805) {
+    if (img.file_header.type[0] != 19778 && img.file_header.type[0] != 9805){
         printf("Invalid BMP file\n");
-
-
+        return img;
     }
     fread(&img.info, 1, sizeof(img.info), f);
+    unsigned int w =(int) (3* img.info.width);
 
+
+    img.data = (RGB**)calloc( ((img.info.height*3-3)&(-4)), sizeof(RGB*));
     img.data = (RGB**)malloc(sizeof(RGB*)*((img.info.height*3-3)&(-4)));
     for (int i = 0; i < sizeof(img.info.height);  i++){
         img.data[i] = (RGB*)malloc(sizeof(RGB)*(img.info.width));
         fread(&img.data[i], 1, sizeof(img.data)*img.info.width, f);
     }
-
-
     fclose(f);
-
     return img;
-}
 
+}
 
 void color_inversion(BMP *img, int coord_x1, int coord_y1, int coord_x2, int coord_y2){
     for (int i = coord_y1; i < coord_y2; i++){
