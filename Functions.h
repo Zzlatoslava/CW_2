d1e2c09074d53875d11cbf44a122af3e245da8a6
Zzlatoslava <zlatoslava0215@gmail.com>
2023-05-12 22:16:44 +0300

    Last version

--- Functions.h
+++ Functions.h
@@ -7,9 +7,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Structures.h"
+#include <stdint.h>
+#include <math.h>
+
 
 void save_img (const char* path, BMP img );
 BMP open_img (const char* path);
-void color_inversion(BMP *img, int coord_x1, int coord_y1, int coord_x2, int coord_y2);
+/*void color_inversion(BMP *img, int coord_x1, int coord_y1, int coord_x2, int coord_y2);
+void black_white(BMP * img,int coord_x1, int coord_y1, int coord_x2, int coord_y2 );*/
 
 #endif //CW_2_FUNCTIONS_H
